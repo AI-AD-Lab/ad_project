90e75189db6fb1f6ae52f4ad7405e7c581fc5c61
AI-AD-LAB <smku2025@tukorea.ac.kr>
2025-07-02 22:32:54 +0900

    Before Refactoring

--- rule_based_classification.py
+++ rule_based_classification.py
@@ -20,23 +20,24 @@ from rule_utils.u_turn import detect_u_turn
 
 #%%
 
-COLUMN_NAMES = config['data_columns']
+
 # SAMPLING_RATE = config['sampling_rate']
 
 def data_load(data_file_path):
+    """ Load data from a CSV file and normalize the time column. """
+    USEDCOLUMNS = config['data_columns']
 
-    """
-    Load data from a CSV file and normalize the time column.
-    """
     if not os.path.exists(data_file_path):
         raise FileNotFoundError(f"The file {data_file_path} does not exist.")
 
     # Load the data
-    data = pd.read_csv(data_file_path)
-    data = data[COLUMN_NAMES]
-    return data
+    _data = pd.read_csv(data_file_path)
+    _data = _data[USEDCOLUMNS]
+    return _data
 
 def pandas_plot_save(df, save_path:None|str=None):
+    ''' Render a pandas DataFrame as a table and save or show it. '''
+
     fig, ax = plt.subplots(figsize=(5, 2))
 
     # 테이블 렌더링
@@ -56,6 +57,18 @@ def pandas_plot_save(df, save_path:None|str=None):
     plt.close(fig)
 
 
+# def excute_rule_based_classification(perm:list[str]) -> None:
+#     """
+#     Execute the rule-based classification for trajectory types.
+#     This function processes the data, applies classification rules, and saves the results.
+#     """
+
+#     labeled_data = [[] for _ in range(len(cls_label))]
+#     real_index = { short_to_long_label[label]:idx for idx, label in enumerate(perm)}
+
+#     pass  # Placeholder for the main execution logic if needed
+
+
 # 기본 경로 설정
 MORAISIM_PATH = Path(__file__).resolve().parent.parent
 SINGLE_SCENARIO_SYNLOG_DATA_ROOT = MORAISIM_PATH /  'simulation_TOTAL_250626'  # 시간이 일정한 데이터 파일: SYNC
@@ -65,20 +78,12 @@ label_cls = config['label_to_class']
 short_to_long_label = config['Short_to_Long_Label']
 
 label_data = pd.read_csv(SINGLE_SCENARIO_SYNLOG_DATA_ROOT / 'label.csv')
-# labeled_data = [[] for _ in range(len(cls_label))]
-
-re_data = []
-count = 0
-
 labels = ['RA','ST', 'UT', 'LT', 'RT', 'LLC', 'RLC']
 perms = list(itertools.permutations(labels))
 
-#%%
 for perm_idx, perm in enumerate(perms):
     labeled_data = [[] for _ in range(len(cls_label))]
     real_index = { short_to_long_label[label]:idx for idx, label in enumerate(perm)}
-    print(f'perm: {perm}')
-    print(f'real_index: {real_index}')
 
     for file, label in zip(label_data['file_name'], label_data['trajectory type']):
 
@@ -87,8 +92,9 @@ for perm_idx, perm in enumerate(perms):
 
         ST , RT, LT, UT, LLC, RLC, RA = 0, 0, 0, 0, 0, 0, 0
         NO_LABEL = 0
-        counting = 1
+        COUNT = 1
 
+        # predict each label
         LLC = detect_left_lane_change(data, duration_sec=0.7, threshold=0.2)
         RLC = detect_right_lane_change(data, duration_sec=0.7, threshold=0.2)
         ST = detect_straight(data, abs_normal_threshold=0.05, abs_threshold=0.3, duration_sec=8)
@@ -97,8 +103,7 @@ for perm_idx, perm in enumerate(perms):
         RA = detect_roundabout(data)
         UT = detect_u_turn(data)
 
-
-        label_variable = {
+        label_variable = { # update label_variable to match the perm order
             'RA': RA,
             'ST': ST,
             'UT': UT,
@@ -108,15 +113,7 @@ for perm_idx, perm in enumerate(perms):
             'RLC': RLC
         }
 
-        # UT -> LT -> RT -> LLC -> RLC -> RA -> ST // priority sequence
-
-
-        # labels = ['RA','ST', 'UT', 'LT', 'RT', 'LLC', 'RLC']
         values = [ label_variable[label] for label in perm ] # 우선순위에 맞게 정렬, prediction
-        
-        # values = [ label for label in perm ]
-
-        # values = [ RA, ST, UT,  LT, RT, LLC, RLC]
 
         # 첫 번째만 셀렉
         for i, value in enumerate(values) :
@@ -130,25 +127,8 @@ for perm_idx, perm in enumerate(perms):
             result_list = [0] * 9
             result_list[-2] = 1
 
-        result_list[-1] = counting
+        result_list[-1] = COUNT
         labeled_data[real_index[label]].append(result_list) # real-> prediction 넣기
-        # print(f'label: {label_cls[label]}, result: {result_list}')
-
-        # count += 1
-        # if count % 100 == 0:
-        #     print(f'Processed {count} files...')
-    
-
-    # total_result = []
-    # for i, label in enumerate(labeled_data):
-    #     np_sliced = np.array(label)
-    #     np_sliced_change = [[0 if x is None else x for x in row] for row in np_sliced]
-    #     column_sum = np.sum(np_sliced_change, axis=0)
-    #     total_result.append(column_sum)
-
-    # print(['ST', 'RT', 'LT', 'UT', 'LLC', 'RLC', 'RA', "NO_LABEL", "TOTAL"])
-    # df_data = pd.DataFrame(total_result)
-    # print(df_data)
 
     total_result = []
     for i, label in enumerate(labeled_data):
@@ -159,22 +139,13 @@ for perm_idx, perm in enumerate(perms):
 
     perm_array = list(perm)
     df_total_result = pd.DataFrame(total_result, columns=perm_array + ["NO_LABEL", "TOTAL"], index=perm_array)
-    # df_total_result.index = cls_label
-    # print(df_total_result)
 
-    # pandas_save_path = './output/plots/score/'
-    # if not os.path.exists(pandas_save_path):
-    #     os.makedirs(pandas_save_path)
-    # pandas_plot_save(df_total_result, save_path=pandas_save_path + f"total_result_{perm_idx}.png")
-    # df_total_result.to_csv(pandas_save_path + f"total_result_{perm_idx}.csv", index=False)
-
-    print(df_total_result)
-    break
+    pandas_save_path = './output/plots/score/'
+    if not os.path.exists(pandas_save_path):
+        os.makedirs(pandas_save_path)
+    pandas_plot_save(df_total_result, save_path=pandas_save_path + f"total_result_{perm_idx}.png")
+    df_total_result.to_csv(pandas_save_path + f"total_result_{perm_idx}.csv", index=False)
 
     if perm_idx % 100 == 0:
         print(f'Processed {perm_idx} permutations...')
 
-# %%
-
-# UT -> LT -> RT -> LLC -> RLC -> RA -> ST
-# %%
